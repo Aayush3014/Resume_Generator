4639824e4138b90bebddae8087a5ebbafc12736e
Aayush3014 <ayushak3008@gmail.com>
2023-08-29 00:57:26 +0530

    Taking data from user in form

--- resume/views.py
+++ resume/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-
+from .models import ProfileFields
 # Create your views here.
 
 def index(request):
+
+    if request.method == "POST":
+        name = request.POST.get("name","")
+        email = request.POST.get("email","")
+        phone = request.POST.get("phone","")
+        summary = request.POST.get("summary","")
+        degree = request.POST.get("degree","")
+        school = request.POST.get("school","")
+        university = request.POST.get("university","")
+        experience = request.POST.get("experience","")
+        skills = request.POST.get("skills","")
+
+
+        profile = ProfileFields(name=name,summary=summary,phone=phone,school=school,university=university,degree=degree,email=email,experience=experience,skills=skills)
+
+        profile.save()    
     return render(request, "resume/index.html")
